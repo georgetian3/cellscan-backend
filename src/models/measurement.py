437741356c5a686d401395769f6c52b90bd7b206
George Tian <github@georgetian.com>
2024-03-05 14:31:22 +0800

    update measurement model

--- src/models/measurement.py
+++ src/models/measurement.py
@@ -4,14 +4,13 @@ from sqlmodel import Field, SQLModel
 
 class MeasurementUpload(SQLModel):
     time_measured: datetime
-    longitude: float
-    latitude: float
-    altitude: float
+    location: str = Field(sa_column=Column(UnicodeText(1000)))
     # large number required to create LONGTEXT in MySQL
-    cell_info: str = Field(sa_column=Column(UnicodeText(32000000)))
+    cells: str = Field(sa_column=Column(UnicodeText(32000000)))
 
 class Measurement(MeasurementUpload, table=True):
     __tablename__ = 'measurement'
     id: int = Field(primary_key=True)
     time_uploaded: datetime
     ip: str
+
