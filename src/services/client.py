115e7202ffcd21a611cb254ccb71346539eb6640
George Tian <github@georgetian.com>
2024-03-12 14:57:17 +0800

    apk client

--- src/services/client.py
+++ src/services/client.py
@@ -1,26 +1,30 @@
 import json
-
+import pathlib
+import aiohttp
 from fastapi.responses import FileResponse
 from models.client import Client
 from services.base_service import BaseService
-import aiohttp
-import pathlib
+
 
 class ClientService(BaseService):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.latest_version: str = None
-
-    async def get_latest_apk(self) -> Client:
+    async def get_latest_apk(self) -> FileResponse | None:
         async with aiohttp.ClientSession() as session:
+            # get latest release info
             async with session.get('https://api.github.com/repos/georgetian3/cellscan-app/releases/latest') as response:
                 data = json.loads(await response.text())
-            latest_apk_name = data['assets'][0]['name']
+            print(data)
+            try:
+                latest_apk_name = data['assets'][0]['name']
+            except:
+                print('bad json')
+                return
+            # download latest release
             if not pathlib.Path(latest_apk_name).is_file():
                 async with session.get(data['assets'][0]['browser_download_url']) as response:
                     with open(latest_apk_name, 'wb') as f:
                         f.write(await response.read())
+            # return APK file
             return FileResponse(
                 latest_apk_name,
                 filename=latest_apk_name,
